8796da26eec71a4f8f38cc10da4b00fbc3f3d0bd
Parth Lamba <parthlamba2007@gmail.com>
2026-08-02 12:56:34 +0530

    added features in app.py

--- app.py
+++ app.py
@@ -51,3 +51,88 @@ valid_matches = matches[matches["winner"].notna()].copy()
 valid_matches["toss_won_match"] = (
     valid_matches["toss_winner"] == valid_matches["winner"]
 )
+
+# ----------------------------
+# Sidebar
+# ----------------------------
+st.sidebar.title("🏏 IPL Dashboard")
+st.sidebar.markdown("Explore IPL data from **2008–2024**")
+
+page = st.sidebar.radio(
+    "Navigation",
+    [
+        "🏠 Overview",
+        "🏟️ Match Analysis",
+        "🏏 Batting Analysis",
+        "🎯 Bowling Analysis",
+        "📈 Season Trends"
+    ]
+)
+#Season Filter
+st.sidebar.markdown("---")
+st.sidebar.subheader("Filters")
+
+season_options = ["All"] + sorted(matches["season"].unique().tolist())
+
+selected_season = st.sidebar.selectbox(
+    "Season",
+    season_options
+)
+
+#Team Filter
+teams = sorted(matches["winner"].dropna().unique())
+
+team_options = ["All"] + teams
+
+selected_team = st.sidebar.selectbox(
+    "Team",
+    team_options
+)
+
+#Creating filtered datsets
+filtered_matches = matches.copy()
+filtered_deliveries = deliveries.copy()
+filtered_merged = merged.copy()
+
+#Applying season filter
+if selected_season != "All":
+
+    filtered_matches = filtered_matches[
+        filtered_matches["season"] == selected_season
+    ]
+
+    match_ids = filtered_matches["id"]
+
+    filtered_deliveries = filtered_deliveries[
+        filtered_deliveries["match_id"].isin(match_ids)
+    ]
+
+    filtered_merged = filtered_merged[
+        filtered_merged["season"] == selected_season
+    ]
+
+#Applying team filter
+if selected_team != "All":
+
+    filtered_matches = filtered_matches[
+        (filtered_matches["team1"] == selected_team) |
+        (filtered_matches["team2"] == selected_team)
+    ]
+
+    match_ids = filtered_matches["id"]
+
+    filtered_deliveries = filtered_deliveries[
+        filtered_deliveries["match_id"].isin(match_ids)
+    ]
+
+    filtered_merged = filtered_merged[
+        (filtered_merged["batting_team"] == selected_team) |
+        (filtered_merged["bowling_team"] == selected_team)
+    ]
+
+filtered_matches
+
+filtered_deliveries
+
+filtered_merged
+
